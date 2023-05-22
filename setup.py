3df9b2df64be9e35e37e1bd86445edda1f483dd7
Carsten Wulff <carsten@wulff.no>
2023-05-22 19:42:29 +0200

    incremented version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cicpy",
-    version="0.1.5+",
+    version="0.1.6+",
     author="Carsten Wulff",
     author_email="carsten@wulff.no",
     description="Custom IC Creator Python Frontend",
